4963c8e947eee63c77b0456956ba769aab5887b4
Bhavy Zala <126322584+Bhavyzala@users.noreply.github.com>
2023-12-23 21:19:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const API_KEY = '2f3eccaa7f2ca4ef1690aa4d99b84a08';
+const form = document.querySelector('form');
+const search = document.querySelector('#search');
+const weather = document.querySelector('#weather');
+
+const getWeather = async (city) => {
+  weather.innerHTML = '<h2>Loading...</h2>';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    showWeather(data);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    weather.innerHTML = '<h2>City Not Found</h2>';
+  }
+};
+
+const showWeather = (data) => {
+  if (data.cod === '404') {
+    weather.innerHTML = '<h2>City Not Found</h2>';
+    return;
+  }
+  weather.innerHTML = `
+    <div>
+      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+    </div>
+    <div>
+      <h2>${data.main.temp} ℃</h2>
+      <h4>${data.weather[0].main}</h4>
+    </div>
+  `;
+};
+
+form.addEventListener('submit', function (event) {
+  getWeather(search.value);
+  event.preventDefault();
+});
